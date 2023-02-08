56f70a497915b21a1e72ba9dfc15353158c3e63a
YJ <69757372+yujade01@users.noreply.github.com>
2023-02-08 19:35:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,6 @@
-import numpy as np
-import altair as alt
-import pandas as pd
 import streamlit as st
+from datetime import time, datetime
 
-st.header('st.write')
+st.header('Shooting Score Analysis of NBA Players 16-17')
 
-# Example 1
-
-st.write('Hello, *World!* :sunglasses:')
-
-# Example 2
-
-st.write(1234)
-
-# Example 3
-
-df = pd.DataFrame({
-     'first column': [1, 2, 3, 4],
-     'second column': [10, 20, 30, 40]
-     })
-st.write(df)
-
-# Example 4
-
-st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
-
-# Example 5
-
-df2 = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(c)
+st.subheader('NBA players')
