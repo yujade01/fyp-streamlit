9ec0f8ecc16c82d378b6592ed154b6471bc761e4
YJ <69757372+yujade01@users.noreply.github.com>
2023-02-08 18:59:18 +0800

    Delete streamlit.py

--- streamlit.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-
-df_location = pd.read_csv("location.csv")
-
-customer_location = df_location[['latitude', 'longitude']]
-st.map(customer_location)
