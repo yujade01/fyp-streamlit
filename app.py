5158e9a8937063df8aca032216b563c730b12952
YJ <69757372+yujade01@users.noreply.github.com>
2023-02-09 19:38:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,14 +50,6 @@ else:
 if(nba_team != ' ' and nba_player != ' '):
     st.header('Results')
 
-    my_bar = st.progress(0)
-
-    for percent_complete in range(100):
-        time.sleep(0.05)
-        my_bar.progress(percent_complete + 1)
-
-    st.balloons()
-
     #st.write(selected_team_players)
 
     fullname = str(nba_player)
